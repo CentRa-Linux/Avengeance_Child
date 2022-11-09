b06dd7522ba17bcb8930d41339cd3bb7952da63d
Centra <centra-linux@protonmail.com>
2022-11-09 15:37:34 +0900

    Second Commit

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,13 @@ void setup() {
   Serial.begin(9600);
   Serial.println(
       "This is Arduino Micro, Sending message to PC on Serial Connection.");
+
+  Serial.println("Setting up i2c...");
+  Wire.begin(0x08);
+  Wire.onRequest(requestEvent);
+  Serial.println("i2c Ok!");
 }
 
 void loop() {}
+
+void requestEvent() {}
