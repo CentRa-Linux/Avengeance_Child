abf40795b2874f4e560daaedd528f870e5ca92fa
Centra <centra-linux@protonmail.com>
2022-11-09 11:55:03 +0900

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+// Avengeance's child program for Arduino Micro
+
+#include <Arduino.h>
+#include <Wire.h>
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println("This is ESP32, Sending message to PC on Serial Connection.");
+}
+
+void loop() {
+}
