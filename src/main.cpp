1e89bd4d1d1c260743a9d39a5da5710e6443134f
Centra <centra-linux@protonmail.com>
2022-11-09 11:55:51 +0900

    Fixed typo

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,9 @@
 #include <Wire.h>
 
 void setup() {
-    Serial.begin(9600);
-    Serial.println("This is ESP32, Sending message to PC on Serial Connection.");
+  Serial.begin(9600);
+  Serial.println(
+      "This is Arduino Micro, Sending message to PC on Serial Connection.");
 }
 
-void loop() {
-}
+void loop() {}
